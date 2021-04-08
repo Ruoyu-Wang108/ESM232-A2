a3d5dd274b1d6079bf115cc9fc4e397dde737de8
Ruoyu-Wang108 <865524384@qq.com>
2021-04-08 09:22:59 -0700

    updated the documentation of the function script

--- R/almond_yield_anomaly.R
+++ R/almond_yield_anomaly.R
@@ -1,18 +1,22 @@
 #' Almond Yield
 #'
 #' @description
+#' This function calculates the anomaly Almond yield based on minimum temperature in February and total precipitation in January.
 #'
-#' @author
+#' @author Margaret Brickner, Nathalie Eegholm, Ruoyu Wang
 #'
-#' @param
+#' @param feb_min_t The minimum temperature in February. Unit: Celcius.
+#' @param jan_precip The total precipitation in January. Unit: millimeter.
+#' @param var_feb_min_t_1 The coefficient associated with feb_min_t. Default: -0.015
+#' @param var_feb_min_t_2 The coefficient associated with feb_min_t square. Default: -0.0046
+#' @param var_jan_precip_1 The coefficient associated with jan_precip. Default: -0.07
+#' @param var_jan_precip_2 The coefficient associated with jan_precip square. Default: 0.0043
+#' @param intercept The intercept of this statistic model. Default: 0.28
 #'
-#' @return
+#' @return Almond yield anomaly. Unit: ton/acre.
 #'
-#' @references
-#'
-#'
-#' @examples
-#'
+#' @references Lobell et al., 2006
+#' @examples almond_yield_anomaly(3, 5)
 
 
 almond_yield_anomaly <- function(feb_min_t,
